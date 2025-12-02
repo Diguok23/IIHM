769cb78ff73db2a053403f9da6ec41ca235054c5
Diguok23 <addijenwriters001@gmail.com>
2025-12-02 03:04:52 -0800

    Update page.tsx

--- app/apply/page.tsx
+++ app/apply/page.tsx
@@ -17,8 +17,8 @@ export default function Apply() {
           <div className="container mx-auto px-4 sm:px-6 lg:px-8">
             <h1 className="text-4xl font-bold mb-4">Application Form</h1>
             <p className="text-xl max-w-3xl">
-              Complete the form below to apply for certification programs at the American Professional Management
-              Institute of Hospitality.
+              Fill out the form below to apply for certification programs at the International Institute of Hospitality
+              Management (IIHM).
             </p>
           </div>
         </div>
